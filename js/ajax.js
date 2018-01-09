aa189401595b8284ee49286b2e542fa771e5fd01
Gabi Mena <gabriela.grojas@gmail.com>
2018-01-09 18:04:36 +0100

    responsive tablet

--- js/ajax.js
+++ js/ajax.js
@@ -1,12 +1,33 @@
-//Server request
-function requestServer(fn) {
-    var request = new XMLHttpRequest();
-    request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh');
+// //Server request
+// function requestServer(fn) {
+//     var request = new XMLHttpRequest();
+//     request.open('GET', 'https://three-random-reasons-izwfjpgbqm.now.sh');
+//
+//     //Load event when data is ready to use
+//     // request.addEventListener('load', printReason);
+//     request.addEventListener('load', fn);
+//
+//     //Send request
+//     request.send();
+// }
 
-    //Load event when data is ready to use
-    // request.addEventListener('load', printReason);
-    request.addEventListener('load', fn);
+//Promises
+var btnMoreReasons = document.querySelector('.btn-more-reasons');
 
-    //Send request
-    request.send();
+function requestServer() {
+    fetch('https://three-random-reasons-izwfjpgbqm.now.sh')
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(json){
+        var reasons = json.reasons;
+        var listHTML = '';
+        for (var i = 0; i < reasons.length; i++) {
+          listHTML += '<div><h2>' + reasons[i].title + '</h2><p>' + reasons[i].description + '</p></div>';
+        }
+        var grid += document.querySelector('.grid-reasons');
+        grid.innerHTML = listHTML;
+      });
 }
+
+btnMoreReasons.addEventListener('click' function);
