81a9bea1e41672dec218944d1c2a5421a8b25989
Gabi Mena <gabriela.grojas@gmail.com>
2018-01-09 14:38:02 +0100

    add menu hidden and styles

--- js/header.js
+++ js/header.js
@@ -2,7 +2,11 @@
 var body = document.querySelector('body');
 var btnMenu = document.querySelector('.btn-menu');
 var btnClose = document.querySelector('.btn-close');
+var links = document.querySelectorAll('.menu-link');
 
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', closeNavMenu);
+}
 // Functions
 function openNavMenu() {
 body.classList.add('menu-show');
